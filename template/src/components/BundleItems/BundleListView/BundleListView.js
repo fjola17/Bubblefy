4aa841c2b0c4fddd1474e5ef3783b859a9e2dc3b
skandalabrandur <steinerinn@gmail.com>
2019-03-06 17:55:28 +0000

    Smá fix fyrir böndúl

--- template/src/components/BundleItems/BundleListView/BundleListView.js
+++ template/src/components/BundleItems/BundleListView/BundleListView.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import BundleListViewItem from '../BundleListViewItem/BundleListViewItem';
+import { Link } from 'react-router-dom';
 
 const BundleLstView = (props) =>{
+    console.log(props);
     return(
         <ul className="list-view">
-            {props.bundles.map(item=><BundleListViewItem key={item.id} bundle={item}/>)}
+            {props.bundles.map(
+                bundle => {
+                    return <Link to={`/bundles/${bundle.id}`}>{`${bundle.name}`}</Link>
+                }
+            )}
         </ul>
     )
 }
