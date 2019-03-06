f9f6bca474df545fbfd2a26d82d5e4f85bde66b6
skandalabrandur <steinerinn@gmail.com>
2019-03-06 14:13:44 +0000

    Getting info on bubbles/:id

--- template/src/components/BubbleItems/ProductItem/ProductItem.js
+++ template/src/components/BubbleItems/ProductItem/ProductItem.js
@@ -1,55 +1,14 @@
 import React from 'react';
-const getBubbles = () =>{
-    return {products : [        {
-        id: 1,
-        name: "Plain bubbles",
-        description: "These are the plain bubbles! They are as plain as they get!",
-        price: 499,
-        image: "https://i.imgur.com/ZiNsJOp.png"
-    },
-    {
-        id: 2,
-        name: "Wild bubbles",
-        description: "These are the wild bubbles! They form various shapes as found in the wild! Look out! It's a bubble bear!",
-        price: 799,
-        image: "https://i.imgur.com/w9Usvcd.png"
-    },
-    {
-        id: 3,
-        name: "Triple bubbles",
-        description: "These are the triple bubbles! A guarantee of three bubbles each blow and it never fails! Never!",
-        price: 599,
-        image: "https://i.imgur.com/5ugrK8x.png"
-    },
-    {
-        id: 4,
-        name: "Bubblegum bubba-bubbles",
-        description: "The infamous Bubblegum bubba-bubbles! They smell like bubblegum and they even taste like bubblegum! If you manage to catch them, you can eat them and they will turn into bubblegum! Amazing!",
-        price: 999,
-        image: "https://i.imgur.com/XoA1hDZ.png"
-    },
-    {
-        id: 5,
-        name: "Dark matter",
-        description: "The Dark Matter bubbles are unique and a new hit! They are black as the night and heavy as well, so they don't float! They sink!",
-        price: 899,
-        image: "https://i.imgur.com/3kIIn3i.png"
-    },
-    {
-        id: 6,
-        name: "Christmas bubbles",
-        description: "The seasonal Christmas bubbles are right around the corner! They come in white, green and red!",
-        price: 699,
-        image: "https://i.imgur.com/UoqLLcB.png"
-    }]}
-}
+import { apifetch } from '../../../services/ApiFetcher';
 class ProductItem extends React.Component{
     
     componentDidMount() {
-        /*ATH: VIRKAR EKKI OG ÞARF AÐ LAGA
-        var productItem = getBubbles.find((item)=> item.id =this.props.match.params.product);
-        console.log(productItem);
-        this.setState({productItem:productItem});*/
+        const id = this.props.match.params.BubbleId;
+        apifetch(`bubbles/${id}`)
+        .then(request => request.json())
+        .then(data => this.setState({products: data}))
+        
+        console.log(id);
     }
     constructor(){
         super();
@@ -57,13 +16,11 @@ class ProductItem extends React.Component{
             products: []
         }
     }
-    render(){
-        console.log("hallo");
+    render(match){
         return (
-        
-        <div><p>No products can be seen right now, I'm just a placeholder </p>
-        <button>Add to cart</button>
-        </div>
+            <div>
+                { JSON.stringify(this.state.products) }
+            </div>
         )
     }
 }
