d43ea193f74926d85299596c0249824f4457da9f
skandalabrandur <steinerinn@gmail.com>
2019-03-07 15:52:09 +0000

    marshalling

--- template/src/services/Storage.js
+++ template/src/services/Storage.js
@@ -12,20 +12,43 @@ const productIncrement = (productName) => {
     }
 }
 
+// The marshalls preserve objects that may be in localStorage for other various reasons
+// This is done with the bubbles:: prefix
 const marshallStorage = () => {
     var keys = Object.keys(localStorage).filter(
         (key) => {
             return key.includes("bubbles::")
         }
     )
-
     var relevantStorage = {};
-
     for(i = 0; i < keys.length; i++) {
         relevantStorage[keys[i]] = localStorage[keys[i]];
     }
 
     return JSON.stringify(relevantStorage)
+}
+
+const unmarshallStorage = (json) => {
+    var storedObject = JSON.parse(json);
+    var keys = Object.keys(storedObject).filter(
+        (key) => {
+            return key.includes("bubbles::")
+        }
+    )
+    
+    var previousKeys = Object.keys(localStorage).filter(
+        (key) => {
+            return key.includes("bubbles::")
+        }
+    )
+
+    for(i = 0; i < previousKeys.length; i++) {
+        localStorage.removeItem(previousKeys[i])
+    }
+
+    for(i = 0; i < keys; i++) {
+        localStorage.setItem(key, storedObject[key]);
+    }
 }
 
 export {
