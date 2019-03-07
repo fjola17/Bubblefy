3700590f1b2be988d7226f32ba194f7b4d605718
fjola17 <34029285+fjola17@users.noreply.github.com>
2019-03-07 13:14:54 +0000

    form 1 works with error handling

---
+++ template/src/components/Checkout/StoreInput/StoreInput.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import propTypes from 'prop-types';
+
+const StoreInput = props => {
+    const { onInput, type, errorMessage, label, name} = props
+
+    return(
+        <div className="form-group">
+        {
+        label
+        ?
+        <label  className="control-label">{ label }</label>
+        :
+        <></>
+        }
+        <input
+        type={ type }
+
+        onChange={ onInput }
+        name={ name }
+        
+        className="form-control" />
+        <span className="error">{ errorMessage }</span>
+    </div>
+    )
+}
+StoreInput.propTypes = {
+    
+    name: propTypes.string.isRequired,
+  onInput: propTypes.func.isRequired,
+  type: propTypes.oneOf([ 'text', 'number', 'password', 'submit', 'email' ]),
+  errorMessage: propTypes.string,
+  label: propTypes.string
+}
+export default StoreInput;
--- template/src/components/Checkout/StorePickup/StorePickup.js
+++ template/src/components/Checkout/StorePickup/StorePickup.js
@@ -2,6 +2,7 @@ import React from 'react'
 import validator from 'validator';
 import toastr from 'toastr';
 import StoreForm from '../StoreForm/StoreForm';
+import StoreInput from '../StoreInput/StoreInput';
 
 class StorePickup extends React.Component{
     constructor(props){
@@ -29,10 +30,10 @@ class StorePickup extends React.Component{
         const {fullName, phoneNumber} = this.state.fields;
         const errors = {}
         if(fullName === ""){
-            errors.fullNameError = "Full name is required";
+            errors.fullNameError = "Error: Full name is required!";
         }
         if(phoneNumber === ""){
-            errors.phoneNumberError = "phone number is required";
+            errors.phoneNumberError = "Error: Phone number is required";
         }
         if(Object.keys(errors).length > 0){
             this.setState({...this.state.errors, errors});
@@ -57,8 +58,8 @@ class StorePickup extends React.Component{
 
         return(
             <StoreForm onSubmit={e => this.submitForm(e)}>
-            <div>Name: <input className="form-control" type="text" name="fullName" htmlid="fullName" label="enter full name" placeholder="enter your name" errormessage={fullNameError} onInput={e=>this.onInput(e)} /></div>
-            <div>Phone number: <input type="number" className="form-control" name="phoneNumber" htmlid="phoneNumber" label="enter your phone number" placeholder="enter your phonenumber" errormessage={phoneNumberError} onInput = {e=>this.onInput(e)} /></div>
+            <StoreInput type="text" name="fullName"  label="enter full name" errorMessage={fullNameError} onInput={e=>this.onInput(e)} />
+            <StoreInput type="number" name="phoneNumber"  label="enter your phone number" errorMessage={phoneNumberError} onInput = {e=>this.onInput(e)} />
             <input type="submit" value="Proceed" className="btn btn-primary"/>
             </StoreForm>
         )
