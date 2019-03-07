83551562d3a676c3eaecf3be44dceef03a8262ad
skandalabrandur <steinerinn@gmail.com>
2019-03-07 23:22:43 +0000

    Proptyped orderlistviewitem

--- template/src/components/OrderItems/OrderListViewItem/OrderListViewItem.js
+++ template/src/components/OrderItems/OrderListViewItem/OrderListViewItem.js
@@ -1,8 +1,11 @@
 /* Displays order item */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+
 
 const OrderListViewItem = (props) =>{
+    const { bubbles, counter, telephone } = props;
     var lastOrder = () => {
         if(props.counter == 0) {
             return <h4><Link to={`/lastOrder/${props.telephone}`}>Order Last Order</Link></h4>
@@ -21,6 +24,14 @@ const OrderListViewItem = (props) =>{
             }
         </li>
     )
+}
+
+OrderListViewItem.propTypes = {
+    props: PropTypes.shape({
+        bubbles: PropTypes.object,  // Contains information about the ordered products and their quantities
+        counter: PropTypes.number,  // If this becomes 0 then it means that it is the last order
+        telephone: PropTypes.string // Routes to the orderee's telephone for making the same order again
+    })
 }
 
 export default OrderListViewItem;
