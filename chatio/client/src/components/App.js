a2ad9c2a88ed94168d1202e6fc4ec061fe4cd3b8
skandalabrandur <steinerinn@gmail.com>
2019-03-20 05:58:54 +0000

    Indentation fix

--- chatio/client/src/components/App.js
+++ chatio/client/src/components/App.js
@@ -3,29 +3,28 @@ import ChatWindow from './ChatWindow/ChatWindow';
 import SocketContext from '../contexts/SocketContext';
 import UserName from './UserName/UserName';
 
-class App extends React.Component{
-    componentDidMount(){
-
+class App extends React.Component {
+    componentDidMount() {
         const { socket } = this.context;
-    socket.on('users', userList =>{
-        this._populateUserList(userList);
-    });
-    socket.on('rooms', roomList =>{
-        this._populateRooms(roomList);
-    })
- }
- _populateRooms(roomList){
-     this.setState({
-         rooms: roomList.map((r, id) => r)
-     })
- }
- _populateUserList(userList){
-     this.setState({
-         users: userList.map((u, idx) => u)
-     })
- }
-    
-    constructor(props){
+        socket.on('users', userList => {
+            this._populateUserList(userList);
+        });
+        socket.on('rooms', roomList => {
+            this._populateRooms(roomList);
+        })
+    }
+    _populateRooms(roomList) {
+        this.setState({
+            rooms: roomList.map((r, id) => r)
+        })
+    }
+    _populateUserList(userList) {
+        this.setState({
+            users: userList.map((u, idx) => u)
+        })
+    }
+
+    constructor(props) {
         super(props);
         this.state = {
             users: [],
@@ -33,12 +32,12 @@ class App extends React.Component{
         }
     }
 
-    render(){
-        const users={users}
-        const rooms={rooms}
+    render() {
+        const users = { users }
+        const rooms = { rooms }
         console.log(rooms);
         console.log(users);
-        return(
+        return (
             <div className="container">
                 <UserName />
             </div>
--- chatio/client/src/components/UserName/UserName.js
+++ chatio/client/src/components/UserName/UserName.js
@@ -3,60 +3,59 @@ import React from 'react';
 import { connect } from 'react-redux';
 //import SocketContext from '../../contexts/SocketContext';
 import ChatRooms from '../ChatRooms/ChatRooms';
-import { updateUser} from '../../actions/UserActions';
+import { updateUser } from '../../actions/UserActions';
 
-class UserName extends React.Component{
-
-    constructor(props){
+class UserName extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
-            hasName : false,
-            userName : "",
+            hasName: false,
+            userName: "",
             rooms: {}
         }
     }
-    onInput(e){
-        this.setState({[e.target.name] : e.target.value});
+    onInput(e) {
+        this.setState({ [e.target.name]: e.target.value });
     }
-    onFormSubmit(e){
+    onFormSubmit(e) {
         e.preventDefault();
-        if(this.props.userName !== '') {
-            const {userName, rooms} = this.props;
-            const {updateUser} = this.props;
-            updateUser({userName, rooms});
+        if (this.props.userName !== '') {
+            const { userName, rooms } = this.props;
+            const { updateUser } = this.props;
+            updateUser({ userName, rooms });
             this.setState({ hasName: true });
         } else {
             // Toastr display "username can't be blank"?
         }
     }
-    render(){
-        const {userName, hasName} = this.state;
+    render() {
+        const { userName, hasName } = this.state;
         console.log(userName);
-        if(!hasName){
-            return(
+        if (!hasName) {
+            return (
                 <div className="user-name">
-                <h2>Please enter your user name</h2>
-                <div id="user-form">
-                    <form action="submit" onSubmit={ e => this.onFormSubmit(e) }>
-                        <input type="text" name="userName" onInput={ e => this.onInput(e) } placeholder="Please enter your username"/>
-                        <button className="btn btn-primary">Confirm</button>
-                    </form>
+                    <h2>Please enter your user name</h2>
+                    <div id="user-form">
+                        <form action="submit" onSubmit={e => this.onFormSubmit(e)}>
+                            <input type="text" name="userName" onInput={e => this.onInput(e)} placeholder="Please enter your username" />
+                            <button className="btn btn-primary">Confirm</button>
+                        </form>
                     </div>
                 </div>
             );
-        }
-        else {
-            return(
+        } else {
+            return (
                 <div className="chat-rooms"><ChatRooms /></div>
             )
         }
     }
 }
-const mapStateToProps = reduxStoreState =>{
+
+const mapStateToProps = reduxStoreState => {
     console.log(reduxStoreState);
-    return{
+    return {
         "": ""
     }
-
 }
-export default connect(mapStateToProps, {updateUser})(UserName);//connect(mapStateToProps, {updateUser})(UserName);
+
+export default connect(mapStateToProps, { updateUser })(UserName);//connect(mapStateToProps, {updateUser})(UserName);
