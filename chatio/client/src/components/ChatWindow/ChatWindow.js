674cda661c6e84b86c2a34fd54737400091bc4fc
skandalabrandur <steinerinn@gmail.com>
2019-03-21 16:36:54 +0000

    Þér verður ekki lengur kickað ef einhver joinar herbergi sem þú ert ekki í, lol

--- chatio/client/src/components/ChatWindow/ChatWindow.js
+++ chatio/client/src/components/ChatWindow/ChatWindow.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import {Redirect} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {socket} from '../../services/socketService';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { socket } from '../../services/socketService';
 import UserName from '../UserName/UserName';
 import './chatWindow.css';
 import toastr from 'toastr';
@@ -13,43 +13,44 @@ import UserOps from '../UserOps/UserOps';
 class ChatWindow extends React.Component {
     activateListeners() {
         const { RoomName } = this.props.match.params;
-        socket.on('updateusers',(room,users,ops) => {
-            if(Object.keys(users).indexOf(this.props.user.userName) == -1 &&
-                Object.keys(ops).indexOf(this.props.user.userName) == -1) {
-                this.setState({kickOrBan: true})
-            }
-            if(room == RoomName) {
-                this.setState({users: Object.keys(users), ops: Object.keys(ops)});
+        socket.on('updateusers', (room, users, ops) => {
+            if (room == RoomName) {
+                if (Object.keys(users).indexOf(this.props.user.userName) == -1 &&
+                    Object.keys(ops).indexOf(this.props.user.userName) == -1) {
+                    this.setState({ kickOrBan: true })
+                }
+
+                this.setState({ users: Object.keys(users), ops: Object.keys(ops) });
             }
         });
 
-        socket.on('updatechat',(roomName,messageHistory) => {
-            if(roomName===this.state.roomName) {
-                this.setState({messages: messageHistory});
+        socket.on('updatechat', (roomName, messageHistory) => {
+            if (roomName === this.state.roomName) {
+                this.setState({ messages: messageHistory });
             }
         });
 
     }
 
     componentDidMount(props) {
-        const {RoomName}=this.props.match.params;
-        if(this.props.user.userName!=="") {
+        const { RoomName } = this.props.match.params;
+        if (this.props.user.userName !== "") {
             this.activateListeners();
-            socket.emit('joinroom',{room: RoomName},(response,reason) => {
-                if(response) {
-                    this.setState({roomJoined: true, roomName: RoomName});
+            socket.emit('joinroom', { room: RoomName }, (response, reason) => {
+                if (response) {
+                    this.setState({ roomJoined: true, roomName: RoomName });
                 } else {
                     alert(`Couldn't join room because: ${reason}`);
-                    this.setState({kickOrBan: true})
+                    this.setState({ kickOrBan: true })
                 }
             });
         } else {
-            this.setState({redirect: true})
+            this.setState({ redirect: true })
         }
     }
     constructor(props) {
         super(props)
-        this.state={
+        this.state = {
             roomName: "",
             roomJoined: false,
             message: "",
@@ -61,62 +62,62 @@ class ChatWindow extends React.Component {
         }
     }
 
-    sendMessage(keycode=13) {
-        if(this.state.message!==''&&keycode==13) {
-            socket.emit('sendmsg',{roomName: this.state.roomName,msg: this.state.message});
-            this.setState({message: ""});
+    sendMessage(keycode = 13) {
+        if (this.state.message !== '' && keycode == 13) {
+            socket.emit('sendmsg', { roomName: this.state.roomName, msg: this.state.message });
+            this.setState({ message: "" });
         }
     }
 
     onInput(e) {
-        this.setState({[e.target.name]: e.target.value});
+        this.setState({ [e.target.name]: e.target.value });
     }
 
     kickUser(e) {
-        const userName=e.target.id;
-        const roomName=e.target.name;
-        socket.emit('kick',{user: userName,room: roomName},(response) => {
-            if(response) {
+        const userName = e.target.id;
+        const roomName = e.target.name;
+        socket.emit('kick', { user: userName, room: roomName }, (response) => {
+            if (response) {
                 //Alert or toastr success?
             }
         });
     }
 
     banUser(e) {
-        const userName=e.target.id;
-        const roomName=e.target.name;
-        socket.emit('ban', {user: userName, room: roomName}, (response) => {
-            if(response) {
+        const userName = e.target.id;
+        const roomName = e.target.name;
+        socket.emit('ban', { user: userName, room: roomName }, (response) => {
+            if (response) {
 
             }
         });
     }
 
     render() {
-        const {roomJoined,roomName,messages,redirect,users,kickOrBan,ops}=this.state;
+        const { roomJoined, roomName, messages, redirect, users, kickOrBan, ops } = this.state;
         const { userName } = this.props.user;
         let normalUserDisplay = (opStatus) => {
-            if(opStatus) {
+            if (opStatus) {
                 return <ChatWindow.OptionableUsers users={users} roomName={roomName} kickFunc={this.kickUser} banFunc={this.banUser} />
             } else {
                 return <ChatWindow.Users users={users} roomName={roomName} />
             }
         }
 
-        if(redirect||kickOrBan) {
+        if (redirect || kickOrBan) {
             toastr.error("You've been kicked or banned!", "Kick or Ban");
             return (
                 <Redirect to="/" />
             )
         }
 
-        if(roomJoined) {
+        if (roomJoined) {
             return (
                 <div className="chat-window">
                     <ChatWindow.Title roomName={roomName} />
                     <div className="users">
                         <ChatWindow.Ops ops={ops} />
-                        { normalUserDisplay(ops.indexOf(userName) !== -1) }
+                        {normalUserDisplay(ops.indexOf(userName) !== -1)}
                     </div>
                     <ChatWindow.Messages messages={messages} />
                     <div className="input-container">
@@ -132,27 +133,27 @@ class ChatWindow extends React.Component {
         }
     }
 }
-ChatWindow.Title=props => (
+ChatWindow.Title = props => (
     <h3 className="title">{props.roomName}</h3>
 );
 
-ChatWindow.Messages=props => (
+ChatWindow.Messages = props => (
     props.messages.map(msg => <div key={msg.timestamp} className="messages">{msg.nick}: {msg.message}</div>)
 );
 
-ChatWindow.Ops=props => (
+ChatWindow.Ops = props => (
     props.ops.map(op => <div className="user op" key={op}>{op}</div>)
 );
 
-ChatWindow.OptionableUsers=props => (
+ChatWindow.OptionableUsers = props => (
     props.users.map(user => <div className="user" key={user}>{user} <a href="#" id={user} name={props.roomName} onClick={e => props.kickFunc(e)}>KICK</a>  <a href="#" id={user} name={props.roomName} onClick={e => props.banFunc(e)}> | BAN</a></div>)
 );
 
-ChatWindow.Users=props => (
+ChatWindow.Users = props => (
     props.users.map(user => <div className="user" key={user}>{user}</div>)
 )
 
-const mapStateToProps=(reduxState) => {
+const mapStateToProps = (reduxState) => {
     return reduxState;
 }
 
